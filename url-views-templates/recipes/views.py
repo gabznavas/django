96a78bd218db52504c45cf0c20c3056c26d251c3
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-07 22:59:33 -0300

    separate head

--- url-views-templates/recipes/views.py
+++ url-views-templates/recipes/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 def home_view(request: HttpRequest):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        "name": "gabriel navas"
+    })
