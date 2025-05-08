34c88a7e8f14d7b0f1b5beed0b055fa23ed1f461
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-08 16:28:49 -0300

    add footer

--- url-views-templates/recipes/views.py
+++ url-views-templates/recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 def home_view(request: HttpRequest):
-    dummy_recipes = range(50)
+    dummy_recipes = range(6)
     return render(request, 'recipes/pages/home.html', context={
         "dummy_recipes": dummy_recipes
     })
