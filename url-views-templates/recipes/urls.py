4321f7eeb3324e7c1abab355c6850a760bd1b656
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-07 22:15:04 -0300

    add font awesome icons

--- url-views-templates/recipes/urls.py
+++ url-views-templates/recipes/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse, HttpRequest
-from recipes.views import home_view, contato_view, sobre_view
+from recipes.views import home_view
 
 urlpatterns = [
     path('', home_view),
-    path('sobre/', sobre_view),
-    path('contato/', contato_view),
 ]
--- url-views-templates/recipes/views.py
+++ url-views-templates/recipes/views.py
@@ -2,12 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 def home_view(request: HttpRequest):
-    return render(request, 'recipes/home.html', context={
-        'name': 'John Doe'
-    })
-
-def sobre_view(request: HttpRequest):
-    return render(request, 'recipes/sobre.html')
-
-def contato_view(request: HttpRequest):
-    return render(request, 'recipes/contato.html')
+    return render(request, 'recipes/home.html')
