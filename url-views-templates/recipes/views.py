43597b628db1d1e208b40ba93c28219e07509a0d
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-08 19:19:05 -0300

    fix css

--- url-views-templates/recipes/views.py
+++ url-views-templates/recipes/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+from utils.recipes.factory import make_recipe
+
 def home(request: HttpRequest):
     dummy_recipes = range(6)
     return render(request, 'recipes/pages/home.html', context={
-        "dummy_recipes": dummy_recipes
+        "recipes": [make_recipe() for _ in range(10)]
     })
 
 def recipes(request: HttpRequest, id: int):
     print(id)
     dummy_recipes = range(6)
     return render(request, 'recipes/pages/recipe.html', context={
-        "dummy_recipes": dummy_recipes
+        "recipe": make_recipe()
     })
