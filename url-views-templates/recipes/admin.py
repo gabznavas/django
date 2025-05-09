914f764675033a017638d261a372fe306774c78a
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-09 09:28:55 -0300

    add django admin

--- url-views-templates/recipes/admin.py
+++ url-views-templates/recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Recipe, Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)
