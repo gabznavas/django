b9e23796f8e4a72b45e704b262d29bab5918e16d
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-09 09:30:51 -0300

    add other to register

--- url-views-templates/recipes/admin.py
+++ url-views-templates/recipes/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import Recipe, Category
 
+# primeira forma de registrar
 class CategoryAdmin(admin.ModelAdmin):
     ...
+admin.site.register(Category, CategoryAdmin)
 
+# segunda forma de registrar
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     ...
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Recipe, RecipeAdmin)
