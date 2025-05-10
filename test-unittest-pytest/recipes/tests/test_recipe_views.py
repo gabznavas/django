7a8726ce026eaa0dafd610543f3607cebf78a2be
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-10 18:27:41 -0300

    create test recipe base

--- test-unittest-pytest/recipes/tests/test_recipe_views.py
+++ test-unittest-pytest/recipes/tests/test_recipe_views.py
@@ -1,10 +1,13 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Category, Recipe, User
+from recipes.models import Recipe
+from .test_recipe_base import RecipeTestBase
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
+    def tearDown(self):
+        return super().tearDown()
+
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(
@@ -41,104 +44,59 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, template_expected)
 
     def test_recipe_home_template_shows_no_found_if_no_recipes(self):
+        for recipe in Recipe.objects.all():
+            recipe.delete()
         response = self.client.get(reverse('recipes:home'))
         received = response.content.decode('utf-8')
         expected = 'No recipes found here'
         self.assertIn(expected, received)
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='category 1')
-        user = User.objects.create(
-            username='test', password=123, email='test@test.com'
-        )
-        Recipe.objects.create(
-            title='any title',
-            author=user,
-            category=category,
-            description='any description',
-            preparation_steps=50,
-            preparation_steps_is_html=False,
-            is_published=True,
-            preparation_time=50,
-            preparation_time_unit='Horas',
-            servings=20,
-            servings_unit='Pessoas',
-        )
+
         response = self.client.get(reverse('recipes:home'))
         template_expected = 'recipes/pages/home.html'
         self.assertTemplateUsed(response, template_expected)
 
     def test_recipe_home_template_loads_recipes_context(self):
-        category = Category.objects.create(name='category 1')
-        user = User.objects.create(
-            username='test', password=123, email='test@test.com'
-        )
-        recipe = Recipe.objects.create(
-            title='any title',
-            author=user,
-            category=category,
-            description='any description',
-            preparation_steps='1. any 2. any',
-            preparation_steps_is_html=False,
-            is_published=True,
-            preparation_time=50,
-            preparation_time_unit='Horas',
-            servings=20,
-            servings_unit='Pessoas',
-        )
         response = self.client.get(reverse('recipes:home'))
         context_recipes = response.context['recipes']
-        self.assertEqual(recipe.title, context_recipes[0].title)
-        self.assertEqual(recipe.author.username,
+        self.assertEqual(self.recipe.title, context_recipes[0].title)
+        self.assertEqual(self.recipe.author.username,
                          context_recipes[0].author.username)
-        self.assertEqual(recipe.category.name,
+        self.assertEqual(self.recipe.category.name,
                          context_recipes[0].category.name)
-        self.assertEqual(recipe.description, context_recipes[0].description)
-        self.assertEqual(recipe.preparation_steps,
+        self.assertEqual(self.recipe.description,
+                         context_recipes[0].description)
+        self.assertEqual(self.recipe.preparation_steps,
                          context_recipes[0].preparation_steps)
-        self.assertEqual(recipe.preparation_steps_is_html,
+        self.assertEqual(self.recipe.preparation_steps_is_html,
                          context_recipes[0].preparation_steps_is_html)
-        self.assertEqual(recipe.is_published, context_recipes[0].is_published)
-        self.assertEqual(recipe.preparation_time,
+        self.assertEqual(self.recipe.is_published,
+                         context_recipes[0].is_published)
+        self.assertEqual(self.recipe.preparation_time,
                          context_recipes[0].preparation_time)
-        self.assertEqual(recipe.preparation_time_unit,
+        self.assertEqual(self.recipe.preparation_time_unit,
                          context_recipes[0].preparation_time_unit)
-        self.assertEqual(recipe.servings, context_recipes[0].servings)
-        self.assertEqual(recipe.servings_unit,
+        self.assertEqual(self.recipe.servings, context_recipes[0].servings)
+        self.assertEqual(self.recipe.servings_unit,
                          context_recipes[0].servings_unit)
 
     def test_recipe_home_template_loads_recipes_content(self):
-        category = Category.objects.create(name='category 1')
-        user = User.objects.create(
-            username='test', password=123, email='test@test.com'
-        )
-        recipe = Recipe.objects.create(
-            title='any title',
-            author=user,
-            category=category,
-            description='any description',
-            preparation_steps='1. any 2. any',
-            preparation_steps_is_html=False,
-            is_published=True,
-            preparation_time=50,
-            preparation_time_unit='Horas',
-            servings=20,
-            servings_unit='Pessoas',
-        )
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         recipes = response.context['recipes']
         self.assertEqual(1, len(recipes))
-        self.assertIn(recipe.title, content)
-        self.assertIn(recipe.category.name, content,
+        self.assertIn(self.recipe.title, content)
+        self.assertIn(self.recipe.category.name, content,
                       'category name not found')
-        self.assertIn(recipe.description, content, 'description not found')
-        self.assertIn(str(recipe.preparation_time), content,
+        self.assertIn(self.recipe.description,
+                      content, 'description not found')
+        self.assertIn(str(self.recipe.preparation_time), content,
                       'preparation_time not found')
-        self.assertIn(recipe.preparation_time_unit, content,
+        self.assertIn(self.recipe.preparation_time_unit, content,
                       'preparation_time_unit not found')
-        self.assertIn(str(recipe.servings), content, 'servings not found')
-        self.assertIn(recipe.servings_unit, content,
+        self.assertIn(str(self.recipe.servings), content, 'servings not found')
+        self.assertIn(self.recipe.servings_unit, content,
                       'servings_unit not found')
 
     def test_recipe_category_view_returns_status_404_if_no_recipes_found(self):
