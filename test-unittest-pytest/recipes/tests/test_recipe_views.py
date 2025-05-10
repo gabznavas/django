ff70c5cea714aaaf9b9e7b78bfd5bee205339229
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-10 17:48:48 -0300

    verify content recipe home

--- test-unittest-pytest/recipes/tests/test_recipe_views.py
+++ test-unittest-pytest/recipes/tests/test_recipe_views.py
@@ -107,6 +107,38 @@ class RecipeViewsTest(TestCase):
         self.assertEqual(recipe.servings_unit,
                          context_recipes[0].servings_unit)
 
+    def test_recipe_home_template_loads_recipes_context(self):
+        category = Category.objects.create(name='category 1')
+        user = User.objects.create(
+            username='test', password=123, email='test@test.com'
+        )
+        recipe = Recipe.objects.create(
+            title='any title',
+            author=user,
+            category=category,
+            description='any description',
+            preparation_steps='1. any 2. any',
+            preparation_steps_is_html=False,
+            is_published=True,
+            preparation_time=50,
+            preparation_time_unit='Horas',
+            servings=20,
+            servings_unit='Pessoas',
+        )
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        self.assertIn(recipe.title, content)
+        self.assertIn(recipe.category.name, content,
+                      'category name not found')
+        self.assertIn(recipe.description, content, 'description not found')
+        self.assertIn(str(recipe.preparation_time), content,
+                      'preparation_time not found')
+        self.assertIn(recipe.preparation_time_unit, content,
+                      'preparation_time_unit not found')
+        self.assertIn(str(recipe.servings), content, 'servings not found')
+        self.assertIn(recipe.servings_unit, content,
+                      'servings_unit not found')
+
     def test_recipe_category_view_returns_status_404_if_no_recipes_found(self):
         response = self.client.get(reverse('recipes:category', kwargs={
             "category_id": 999,
