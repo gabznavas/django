8f760d0f03f6bac226fc6ce60abfce39701745fc
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-10 14:57:05 -0300

    test recipe home url is correct

--- test-unittest-pytest/recipes/tests.py
+++ test-unittest-pytest/recipes/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RecipesURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 2, 'Um não é igual a dois'
+    def test_recipe_home_url_is_correct(self):
+        received = reverse('recipes:home')
+        expected = '/'
+        self.assertEqual(received, expected, 'path da home está errada!')
