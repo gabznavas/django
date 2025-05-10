314be3486e8e9a86e5d2033195e2784c08700476
Gabriel Navas <gabnavas.dev@gmail.com>
2025-05-10 12:53:28 -0300

    change project to pt br

--- models-orm/core/settings.py
+++ models-orm/core/settings.py
@@ -105,7 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
